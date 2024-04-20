edc3b486dd994d9f1d2637ac602752a1efee4baf
LinCie <aldiofernanda@protonmail.com>
2024-04-21 00:57:42 +0700

    added local download api

---
+++ src/app/api/download/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import ytdl from "ytdl-core";
+import fs from "fs";
+
+interface PostRequestBody {
+  url: string | null;
+  title: string | null;
+}
+
+export async function POST(req: Request) {
+  try {
+    const { url, title }: PostRequestBody = await req.json();
+
+    const heads = headers();
+
+    const host = heads.get("x-forwarded-host");
+
+    if (!url || !title)
+      return NextResponse.json(
+        { message: "Please provide url and/or title" },
+        { status: 401 }
+      );
+
+    ytdl(url, { quality: "highestaudio" }).pipe(
+      fs.createWriteStream(`public/audio/${title}.mp3`)
+    );
+
+    return NextResponse.json(
+      { url: `${host}/api/download/${encodeURI(title)}` },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { message: "Internal Server Error", err },
+      { status: 400 }
+    );
+  }
+}
